bab91a0b07978cd0f44dde4cb9ef7e3f701d8f29
ivan_masiuk <vavolan@gmail.com>
2023-03-04 18:54:56 +0200

    added django

--- server/server/settings.py
+++ server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1q^%q_88$s4=-th+q52r5x!%)tc21fgb)=4&)+ppip%=rf92u!'
+SECRET_KEY = 'django-insecure-7ft^(m3@t0lu&+e1y=81bccs9m1=qkrnv0xqidjpkgu*t90swt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
