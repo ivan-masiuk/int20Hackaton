034da6cdc5a4ca8175ab099b18f4684c4ef518bb
Penkov Bohdan <webbogdan11@gmail.com>
2023-03-04 20:21:18 +0200

    Added User models and endpoints for working with user

---
+++ server/users/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
+
+from .managers import CustomUserManager
+
+
+class User(AbstractUser):
+    username = None
+    role = models.CharField(blank=False, max_length=50)
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
